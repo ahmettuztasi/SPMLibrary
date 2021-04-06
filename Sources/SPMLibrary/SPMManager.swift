d2bf281c45c814c90f3b73c313381d891ea16051
ahmettuztasi <ahmetuztasi@gmail.com>
2021-04-06 16:57:23 +0300

    added SPMManager

---
+++ Sources/SPMLibrary/SPMManager.swift
@@ -0,0 +1,21 @@
+//
+//  SPMManager.swift
+//  
+//
+//  Created by ahmet on 6.04.2021.
+//
+
+import Foundation
+
+public class SPMManager {
+    public static var shared = SPMManager()
+    
+    init() {
+        
+    }
+    
+    public func getNaturalNumber() -> String {
+        return "0"
+    }
+}
+
